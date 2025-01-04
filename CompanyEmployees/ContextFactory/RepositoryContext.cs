8d116ad26879d8eeaed470ab4df02a28f48183fd
Chukwuemeka Torti <emekatorti@gmail.com>
2025-01-04 13:35:52 -0500

    feat: Added context factory in presentation layer

---
+++ CompanyEmployees/ContextFactory/RepositoryContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using CompanyEmployees.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyEmployees.ContextFactory
+{
+	public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+	{
+		public RepositoryContext CreateDbContext(string[] args)
+		{
+			var options = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			var builder = new DbContextOptionsBuilder<RepositoryContext>()
+				.UseSqlServer(options.GetConnectionString("sqlConnection"));
+
+			return new RepositoryContext(builder.Options);
+
+		}
+	}
+}
