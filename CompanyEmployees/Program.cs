510bffac1d7f7c5345a70d46767ec4de3d9fb22d
Chukwuemeka Torti <emekatorti@gmail.com>
2025-01-04 12:17:18 -0500

    added forward proxy headers to current request, hsts and static file support

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,4 +1,5 @@
 using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,9 +24,15 @@ else
 }
 
 // Configure the HTTP request pipeline.
-app.UseCors("CorsPolicy");
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+	ForwardedHeaders = ForwardedHeaders.All
+});
+
+
+
 
 app.UseAuthorization();
 
