0ecb2211a008857a45ddb4e526460f2cdb8239a9
Chukwuemeka Torti <emekatorti@gmail.com>
2025-01-05 10:34:03 -0500

    feat: persistence layer

---
+++ CompanyEmployees.Core.Services.Abstractions/IServiceManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Core.Services.Abstractions
+{
+	public interface IServiceManager
+	{
+		IEmployeeService EmployeeService { get; }
+		ICompanyService CompanyService { get; }
+	}
+}
