b4f786fb7cbe59285c362b2ac4c987d2e1ea6bb6
Chukwuemeka Torti <emekatorti@gmail.com>
2025-01-05 01:50:19 -0500

    feat: Repository setup in infrastructure project

---
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/CompanyRepository.cs
@@ -0,0 +1,17 @@
+﻿using CompanyEmployees.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Infrastructure.Persistence.Repositories
+{
+	public class CompanyRepository : RepositoryBase<Company>
+	{
+		public CompanyRepository(RepositoryContext repositoryContext)
+			: base(repositoryContext)
+		{
+		}
+	}
+}
---
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using CompanyEmployees.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Infrastructure.Persistence.Repositories
+{
+	public class EmployeeRepository : RepositoryBase<Employee>
+	{
+		public EmployeeRepository(RepositoryContext repositoryContext)
+			: base(repositoryContext)
+		{
+		}
+	}
+}
