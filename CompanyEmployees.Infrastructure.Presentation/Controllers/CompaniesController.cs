d6fab862fb90b805b371ff811bffa814dc99745b
Chukwuemeka Torti <emekatorti@gmail.com>
2025-01-08 05:23:23 -0500

    feat: endpoint to get multiple companies added

--- CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
@@ -39,4 +39,11 @@ public class CompaniesController : ControllerBase
 
 		return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
 	}
+
+	[HttpGet("collection/({ids})", Name = "CompanyCollection")]
+	public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+	{
+		var companies = _serviceManager.CompanyService.GetByIds(ids, trackChanges: false);
+		return Ok(companies);
+	}
 }
